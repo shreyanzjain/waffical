32e86c25886eea965234208311a39796ec759b7e
shreyanzjain <shreyanz32@gmail.com>
2025-06-01 15:39:01 +0530

    add swagger response definition

--- waffical-py/api/user_endpoint.py
+++ waffical-py/api/user_endpoint.py
@@ -6,9 +6,14 @@ from ..services import user_service
 
 router = APIRouter()
 
-@router.post("/users", response_model=user_model.User)
+
+@router.post("/users", response_model=user_model.User,
+             responses={
+                 status.HTTP_400_BAD_REQUEST: {
+                     "description": "Bad Request: User already exists"}
+             })
 async def create_user_endpoint(user: user_model.UserCreate, db: Session = Depends(get_db)):
-    if(user_service.get_user_by_username(db, user.username)):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
-                             detail=f"User {user.username} already exists")
+    if (user_service.get_user_by_username(db, user.username)):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"User {user.username} already exists")
     return user_service.create_user(db, user)

ab1845587c5898f7827dbdaf38b695a1493179b3
shreyanzjain <shreyanz32@gmail.com>
2025-07-25 12:43:37 +0530

    add cors

--- waffical-py/main.py
+++ waffical-py/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from .data_conn import SessionLocal, engine, get_db, Base
 from . import schema
@@ -7,6 +8,13 @@ from .api import user_endpoint, auth_endpoint
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 app.include_router(user_endpoint.router)
 app.include_router(auth_endpoint.router)
 
